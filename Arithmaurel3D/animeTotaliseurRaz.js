3ca87cd986d5e954375a75e6ccb7695c1aceec8e
Nicky Tching <nicky.tching@grenoble-inp.org>
2021-05-12 21:40:00 +0200

    debut animecentre

--- Arithmaurel3D/animeTotaliseurRaz.js
+++ Arithmaurel3D/animeTotaliseurRaz.js
@@ -1,6 +1,7 @@
 import { cadrans, angleInitCadrans, ecrouCentre, renderer, scene, camera } from './initScene.js'
 import { BruitBouton } from './sound.js'
 var nbanimationsRZ = 80;
+var seuil = 5;
 var animeReturnZero;
 
 function razTotaliseur() {
@@ -11,8 +12,21 @@ function razTotaliseur() {
 
 }
 
+function calculSeuil(seuil) {
+    console.log(seuil)
+    for (let i = 0; i < 8; i++) {
+        // si le cadran <= 5
+        if (cadrans[i].rotation.x)
+        // si le cadran > 5
+            cadrans[i].rotation.x -= Math.PI / 10;
+        if (cadrans[i].rotation.x < angleInitCadrans[i]) {
+            cadrans[i].rotation.x = angleInitCadrans[i];
+        }
+    }
+}
 /**
  * faire passer les valeurs entre offset et offset + 2*Math.PI pour l'animation
+ * un pas correspond à Math.PI/9
  */
 function cadransNormal() {
     for (let i = 0; i < 8; i++) {
@@ -31,22 +45,33 @@ function cadransNormal() {
 }
 
 
-
 /**
  * Fonction qui déclenche l'animation de remise a zero de l'écrou au centre
+ *  6 seuils pour le bouton central
+ *  si <= 5, passe à 5, 4, 3, 2, 1, 0
+ *  4.999
+ * si > 5 passe à 5, 6, 7, 8, 9, 10
+ * 5.01
  */
 function animeRazTot() {
-    nbanimationsRZ--;
+    nbanimationsRZ--
     animeReturnZero = requestAnimationFrame(animeRazTot);
+    // partie gauche-droite passe de 80 a 40, seuil pour les frames 74, 68, 62, 56, 50, 44
     if (nbanimationsRZ >= 40) {
+        ecrouCentre.rotation.x -= Math.PI / 100;
+        if (nbanimationsRZ % 6 == 2) {
+            calculSeuil(seuil)
+            seuil--
+        }
         for (let i = 0; i < 8; i++) {
             cadrans[i].rotation.x -= Math.PI / 10;
             if (cadrans[i].rotation.x < angleInitCadrans[i]) {
                 cadrans[i].rotation.x = angleInitCadrans[i];
             }
         }
-        ecrouCentre.rotation.x -= Math.PI / 100;
-    } else {
+    }
+    // partie droite gauche passe de 40 a 0
+    else {
         ecrouCentre.rotation.x += Math.PI / 100;
     }
     // objectParent.parent.rotation.x -= Math.PI/50;
@@ -56,7 +81,7 @@ function animeRazTot() {
     if (nbanimationsRZ == 0) {
         console.log("fini")
         nbanimationsRZ = 80;
-
+        seuil = 5;
         window.cancelAnimationFrame(animeReturnZero);
         BruitBouton.pause()
         BruitBouton.currentTime = 0
